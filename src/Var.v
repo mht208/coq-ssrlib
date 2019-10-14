80f7bcf8d24f07b46dcda1f844e4895c0a73c3c5
Ming-Hsien Tsai <mhtsai208@gmail.com>
2019-10-14 13:26:58 +0800

    Remove a redundant module.

--- src/Var.v
+++ src/Var.v
@@ -41,8 +41,6 @@ Module SSAVarOrder := MakeProdOrdered VarOrder NOrder.
 
 Module SSAVS := FSets.MakeTreeSet SSAVarOrder.
 
-Module SSAVSLemmas := FSetLemmas SSAVS.
-
 Definition svar (x : SSAVarOrder.t) := fst x.
 
 Definition sidx (x : SSAVarOrder.t) := snd x.
@@ -54,7 +52,7 @@ Definition svar_notin v vs : Prop := forall i, ~~ SSAVS.mem (v, i) vs.
 Lemma svar_notin_singleton1 v x :
   svar_notin v (SSAVS.singleton x) -> v != svar x.
 Proof.
-  destruct x as [x i]. move=> /= H; move: (SSAVSLemmas.not_mem_singleton1 (H i)).
+  destruct x as [x i]. move=> /= H; move: (SSAVS.Lemmas.not_mem_singleton1 (H i)).
   move=> Hne; apply/negP => Heq; apply: Hne. rewrite (eqP Heq).
   exact: SSAVS.E.eq_refl.
 Qed.
@@ -63,20 +61,20 @@ Lemma svar_notin_singleton2 v x :
   v != svar x -> svar_notin v (SSAVS.singleton x).
 Proof.
   move/negP=> Hne i. apply/negP => Heq; apply: Hne.
-  move: (SSAVSLemmas.mem_singleton1 Heq) => {Heq}. destruct x as [x j] => /=.
+  move: (SSAVS.Lemmas.mem_singleton1 Heq) => {Heq}. destruct x as [x j] => /=.
   move/eqP => [Hv Hi]. by rewrite Hv.
 Qed.
 
 Lemma svar_notin_union1 v vs1 vs2 :
   svar_notin v (SSAVS.union vs1 vs2) -> svar_notin v vs1.
 Proof.
-  move=> H i. move: (H i) => {H} H. exact: (proj1 (SSAVSLemmas.not_mem_union1 H)).
+  move=> H i. move: (H i) => {H} H. exact: (proj1 (SSAVS.Lemmas.not_mem_union1 H)).
 Qed.
 
 Lemma svar_notin_union2 v vs1 vs2 :
   svar_notin v (SSAVS.union vs1 vs2) -> svar_notin v vs2.
 Proof.
-  move=> H i. move: (H i) => {H} H. exact: (proj2 (SSAVSLemmas.not_mem_union1 H)).
+  move=> H i. move: (H i) => {H} H. exact: (proj2 (SSAVS.Lemmas.not_mem_union1 H)).
 Qed.
 
 Lemma svar_notin_union3 v vs1 vs2 :
@@ -84,21 +82,21 @@ Lemma svar_notin_union3 v vs1 vs2 :
   svar_notin v (SSAVS.union vs1 vs2).
 Proof.
   move=> H1 H2 i. move: (H1 i) (H2 i) => {H1 H2} H1 H2.
-  exact: (SSAVSLemmas.not_mem_union2 H1 H2).
+  exact: (SSAVS.Lemmas.not_mem_union2 H1 H2).
 Qed.
 
 Lemma svar_notin_add1 v x vs :
   svar_notin v (SSAVS.add x vs) -> v != svar x.
 Proof.
   destruct x as [x i] => /= H. move: (H i) => {H} H.
-  move: (SSAVSLemmas.not_mem_add1 H) => {H}. move=> [H _]; apply/negP => Heq.
+  move: (SSAVS.Lemmas.not_mem_add1 H) => {H}. move=> [H _]; apply/negP => Heq.
   apply: H. rewrite (eqP Heq); exact: eqxx.
 Qed.
 
 Lemma svar_notin_add2 v x vs :
   svar_notin v (SSAVS.add x vs) -> svar_notin v vs.
 Proof.
-  move=> H i. move: (H i) => {H} H. move: (SSAVSLemmas.not_mem_add1 H) => {H}.
+  move=> H i. move: (H i) => {H} H. move: (SSAVS.Lemmas.not_mem_add1 H) => {H}.
   move=> [_ H]; exact: H.
 Qed.
 
@@ -111,7 +109,7 @@ Qed.
 Lemma svar_notin_subset v vs1 vs2 :
   SSAVS.subset vs1 vs2 -> svar_notin v vs2 -> svar_notin v vs1.
 Proof.
-  move=> H H2 x. apply/negP => H1. move: (SSAVSLemmas.mem_subset H1 H) => Hmem.
+  move=> H H2 x. apply/negP => H1. move: (SSAVS.Lemmas.mem_subset H1 H) => Hmem.
   move/negP: (H2 x). apply. exact: Hmem.
 Qed.
 
@@ -145,8 +143,8 @@ Lemma new_svar_notin vs : svar_notin (new_svar vs) vs.
 Proof.
   rewrite /new_svar /max_svar. set x := SSAVS.max_elt vs.
   have: SSAVS.max_elt vs = x by reflexivity. case x.
-  - move=> v Hmax i. apply/negP => Hmem. apply: (SSAVSLemmas.max_elt2 Hmax Hmem).
+  - move=> v Hmax i. apply/negP => Hmem. apply: (SSAVS.Lemmas.max_elt2 Hmax Hmem).
     exact: V_ltb_succ.
-  - move=> Hnone i. apply: SSAVSLemmas.is_empty_mem.
-    exact: (SSAVSLemmas.max_elt3 Hnone).
+  - move=> Hnone i. apply: SSAVS.Lemmas.is_empty_mem.
+    exact: (SSAVS.Lemmas.max_elt3 Hnone).
 Qed.
